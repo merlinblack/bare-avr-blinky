5109e9e1fef97d09060723fa20ad8d7b68dee49d
Nigel Atkinson <merlinblack@users.noreply.github.com>
2022-03-03 23:11:11 +1100

    Use timer B3 as it does not have PWM output
    I.e. keep the other timers for doing PWM :-)

--- fancy_blink_4809.c
+++ fancy_blink_4809.c
@@ -5,22 +5,22 @@
 #define TIME_TRACKING_TIMER_COUNT (F_CPU / 1000) // Should correspond to exactly 1 ms, i.e. millis()
 volatile unsigned long timer_millis = 0;
 
-ISR(TCB0_INT_vect)
+ISR(TCB3_INT_vect)
 {
     timer_millis++;
 
     /** clear flag **/
-    TCB0.INTFLAGS = TCB_CAPT_bm;
+    TCB3.INTFLAGS = TCB_CAPT_bm;
 }
 
 void init_timer()
 {
   _PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, 0x00);
-  TCB0.CTRLB     = TCB_CNTMODE_INT_gc;
-  TCB0.CCMP      = TIME_TRACKING_TIMER_COUNT - 1;
-  TCB0.INTCTRL  |= TCB_CAPT_bm;
-  TCB0.CTRLA     = TCB_CLKSEL_CLKDIV1_gc;
-  TCB0.CTRLA    |= TCB_ENABLE_bm;
+  TCB3.CTRLB     = TCB_CNTMODE_INT_gc;
+  TCB3.CCMP      = TIME_TRACKING_TIMER_COUNT - 1;
+  TCB3.INTCTRL  |= TCB_CAPT_bm;
+  TCB3.CTRLA     = TCB_CLKSEL_CLKDIV1_gc;
+  TCB3.CTRLA    |= TCB_ENABLE_bm;
 }
 
 unsigned long millis()
